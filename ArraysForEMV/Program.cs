c6540447d5d23a38665e26eba5d80187f070c0b6
Elliot Vasquez <emvasquez@usf.edu>
2020-10-05 21:16:52 -0400

    V1

--- ArraysForEMV/Program.cs
+++ ArraysForEMV/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿//Author: Elliot Vasquez
+//ISM 4220 Assignment: 3B Arrays
+//Comment: This C# console will demonstrate the execution of an array of 25 elements without user input.
 
-namespace ArraysForEMV
+using System;
+
+namespace ArraysForElliotV
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+            foreach (var item in array)
+            //Display the 25 elements on new lines
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
