5fdf4223ea7cea056e5efef2f3a41adfc9b8bbb8
Elliot Vasquez <emvasquez@usf.edu>
2020-10-05 21:21:55 -0400

    V1

--- ArraysForEMV/Program.cs
+++ ArraysForEMV/Program.cs
@@ -12,7 +12,7 @@ namespace ArraysForElliotV
         {
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
             foreach (var item in array)
-            //Display the 25 elements on new lines
+            //Display the 25 elements on new lines.
             {
                 Console.WriteLine(item);
             }
